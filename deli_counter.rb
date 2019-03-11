08331d1bd3769c0fd0fd89918742c8e569ca5317
Reinald Reynoso <reireynoso@gmail.com>
2019-03-11 17:14:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(array)
   if array.length == 0 
     puts "The line is currently empty."
   else 
-    puts "The line is currently: #{array.chomp!}"
+    puts "The line is currently: #{array.chomp(\n)}"
   end
 end
 
