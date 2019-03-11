523ec72dcf1037238b7b93651ecb47cfea1f59b1
Reinald Reynoso <reireynoso@gmail.com>
2019-03-11 16:56:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+
+def line(array)
+  if array.length == 0 
+    puts "The line is currently empty."
+  else 
+  puts array
+  end
+end
+
+def take_a_number(array,string)
+  if array.length == 0 
+    array.push(string)
+    puts "Welcome, #{string}. You are number 1 in line."
+  else
+    array.push(string)
+    puts "Welcome, #{string}. You are number #{array.length} in line."
+  end
+end
+
+def now_serving(array)
+  if array.length == 0 
+    puts "There is nobody waiting to be served!"
+  else 
+    puts "Currently serving #{array.shift}."
+  end
+end
