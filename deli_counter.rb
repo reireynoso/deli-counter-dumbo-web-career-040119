882aeda5c7afd77369c23f26cb927c548d0bc3eb
Reinald Reynoso <reireynoso@gmail.com>
2019-03-11 17:13:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,12 +4,7 @@ def line(array)
   if array.length == 0 
     puts "The line is currently empty."
   else 
-    puts "The line is currently: "
-    i = 0;
-    until i == array.length
-      print "#{i + 1}. #{array[i]}"
-      i += 1
-    end
+    puts "The line is currently: #{array.chomp}"
   end
 end
 
