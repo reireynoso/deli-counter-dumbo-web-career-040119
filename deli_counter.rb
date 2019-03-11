39e3b94e9f89677b33ab5800fdad2226109cbdd7
Reinald Reynoso <reireynoso@gmail.com>
2019-03-11 17:00:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,9 +4,7 @@ def line(array)
   if array.length == 0 
     puts "The line is currently empty."
   else 
-    array.each do |names| 
-      puts names
-    end
+    puts "The current line is: #{array}"
   end
 end
 
