cb8cf00cf51fdfa9e734ada22b36ccc18c620f07
Reinald Reynoso <reireynoso@gmail.com>
2019-03-11 17:09:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(array)
     puts "The line is currently empty."
   else 
     i = 0;
-    while i == array.length
+    until i == array.length
       puts "The line is currently: #{i + 1}. #{array[i]}"
       i += 1
     end
